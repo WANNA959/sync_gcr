6af69211b884ce33da605484b118e62e9aaa6b30
WANNA959 <wannazjx@163.com>
2023-04-03 23:35:45 -0400

    update

--- sync_image.py
+++ sync_image.py
@@ -1,5 +1,8 @@
 # coding:utf-8
-import subprocess, os
+import subprocess
+import os
+
+
 def get_filename():
     with open("images.txt", "r") as f:
         lines = f.read().split('\n')
@@ -9,31 +12,37 @@ def get_filename():
 # def rename():
 #     name_list= get_filename()
 #     for name in name_list:
-#         new_name = "kenwood/" + name.split("/")[-1]
+#         new_name = "wannazjx/" + name.split("/")[-1]
 #         print(new_name)
 
 
+password = os.environ.get('DOCKER_PASSWORD')
+
+
 def pull_image():
-    name_list= get_filename()
+    name_list = get_filename()
     for name in name_list:
         if 'sha256' in name:
             print(name)
             sha256_name = name.split("@")
             new_name = sha256_name[0].split("/")[-1]
             tag = sha256_name[-1].split(":")[-1][0:6]
-            image = "kenwood/" + new_name + ":"+ tag
+            image = "wannazjx/" + new_name + ":" + tag
             cmd = "docker tag {0}   {1}".format(name, image)
             subprocess.call("docker pull {}".format(name), shell=True)
             subprocess.run(["docker", "tag", name, image])
-            subprocess.call("docker login -u kenwood -p qwer1234", shell=True)
+            subprocess.call("docker login -u wannazjx -p " +
+                            password, shell=True)
             subprocess.call("docker push {}".format(image), shell=True)
         else:
-            new_name = "kenwood/" + name.split("/")[-1]
+            new_name = "wannazjx/" + name.split("/")[-1]
             cmd = "docker tag {0}   {1}".format(name, new_name)
             subprocess.call("docker pull {}".format(name), shell=True)
             subprocess.run(["docker", "tag", name, new_name])
-            subprocess.call("docker login -u kenwood -p qwer1234", shell=True)
+            subprocess.call("docker login -u wannazjx -p " +
+                            password, shell=True)
             subprocess.call("docker push {}".format(new_name), shell=True)
-        
+
+
 if __name__ == "__main__":
     pull_image()
